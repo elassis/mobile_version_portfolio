1812289376d8d39e4f0fa3e17018ab9adfe95d00
enmanuel <enmanuellassis@gmail.com>
2021-06-21 18:26:27 -0400

    cleaning linters

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-let showMenu = () => {
-   
-    let menu = document.querySelector('#mobile-menu');
-    
-    if(menu.classList.contains('display')){
-        menu.classList.remove('display');
-    }
-    else {
-        menu.classList.add('display');
-    }
+function showMenu() {
+  const menu = document.querySelector('#mobile-menu');
+
+  if (menu.classList.contains('display')) {
+    menu.classList.remove('display');
+  } else {
+    menu.classList.add('display');
+  }
 }
