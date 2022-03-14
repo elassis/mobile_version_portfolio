13a0e4925b2ecb289a4a9d6cf8410336d14fb6e1
enmanuel <enmanuellassis@gmail.com>
2022-03-14 14:59:23 -0400

    add description and live urls to projects

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ let projects = [
     title:'car booking application',
     description:"web app where the user can rent cars per hour. in this project I've used react in the frontend and ruby on rails in the backend as a remote rest api.",
     technologies:['rails-icon.png','react-icon.png','posgresql-icon.jpg'],
-    categories:['ror','all'],
-    liveUrl:'whatEver',
-    githubUrl:'github-url'
+    categories:['ror','react','all'],
+    liveUrl:'https://final-capstone-project-r.netlify.app/login',
+    githubUrl:'https://github.com/lfmnovaes/car-rental-back-end'
   },
   {
     title:'rails budget application',
@@ -13,26 +13,44 @@ let projects = [
     image:'url',
     technologies:['rails-icon.png','posgresql-icon.jpg','html-icon.png'],
     categories:['ror','all'],
-    liveUrl:'whatEver',
-    githubUrl:'github-url'
+    liveUrl:'https://calm-thicket-74866.herokuapp.com/',
+    githubUrl:'https://github.com/elassis/rails_budget_app'
+  },
+  {
+    title:'recipes social network',
+    description:'a website where user can create recipes and also share it with other users, user will be able to delete and add as much recipes and food as they want.',
+    image:'url',
+    technologies:['rails-icon.png','posgresql-icon.jpg','html-icon.png'],
+    categories:['ror','all'],
+    liveUrl:'https://enigmatic-escarpment-64320.herokuapp.com/users/sign_in',
+    githubUrl:'github-urlhttps://github.com/LOctavio/rails_recipe_site'
   },
   {
     title:'rockets/missions reservations',
     description:'web application where the user is able to reserve rockets and missions, this application consumes information from the space x remote api.',
     technologies:['js-icon.png','react-icon.png','redux-icon.png'],
     categories:['react','all'],
-    liveUrl:'live-url',
-    githubUrl:'github-url'
+    liveUrl:'https://react-redux-capstone-jose-enmanuel.netlify.app/rockets',
+    githubUrl:'https://github.com/elassis/react-redux-capstone'
   },
   {
-    title:'projectFour',
-    description:'lorem',
+    title:'live statistics about covid-19',
+    description:'a website where users can have live statistical information about covid-19 sorted by country. this information is retrieve from a public API called Narrativa. ',
     image:'url',
-    technologies:['react','javascript','html5'],
+    technologies:['js-icon.png','react-icon.png','redux-icon.png'],
     categories:['react','all'],
-    liveUrl:'whatEver',
-    githubUrl:'github-url'
-  }
+    liveUrl:'https://m3-capstone-microverse.netlify.app/',
+    githubUrl:'https://github.com/elassis/module-3-final-project'
+  },
+  {
+    title:'comment on tv shows website',
+    description:'a website where users can add a comment to the displayed show. this website was made with webpack and consumes data from two remote rest APIs.',
+    image:'url',
+    technologies:['js-icon.png','css-icon.png','html-icon.png'],
+    categories:['all'],
+    liveUrl:'https://elassis.github.io/module-2-capstone/dist/',
+    githubUrl:'https://github.com/elassis/module-2-capstone'
+  },
 ]
 
 const initial = {
